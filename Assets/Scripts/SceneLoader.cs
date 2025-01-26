a11122da44355f25c440126082707e9facf56fdc
shalimma <shalimman@gmail.com>
2025-01-26 01:00:19 -0500

    Made Level2 Scene and changed bg

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private int levelDied = 1;
+
     public static SceneLoader Instance { get; private set; }
     private void Awake()
     {
@@ -22,6 +24,30 @@ public class SceneLoader : MonoBehaviour
     public void GameOver()
     {
         SceneManager.LoadScene("GameOver");
+    }
+
+    public void RetryLevel()
+    {
+        switch (levelDied)
+        {
+            case 1:
+                SceneManager.LoadScene("Level1");
+                break;
+
+            case 2:
+                SceneManager.LoadScene("Level2");
+                break;
+
+            case 3:
+                SceneManager.LoadScene("Level3");
+                break;
+
+        }
+    }
+
+    public void SetLevelDied(int level)
+    {
+        levelDied = level;
     }
 
     void CreateSceneLoader()
