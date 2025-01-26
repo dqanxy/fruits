ee2032fcb8d65492525d4ac4eee5cc82f3a639c0
shalimma <shalimman@gmail.com>
2025-01-26 00:36:46 -0500

    Animated Buttons

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,8 +4,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    public static SceneLoader Instance { get; private set; }
+    private void Awake()
+    {
+        CreateSceneLoader();
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("Level1");
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    void CreateSceneLoader()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
     }
 }
