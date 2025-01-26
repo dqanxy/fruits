3c09d980bc309f83d28d004ab2eb2163ba1d5da3
Marco Brizzio <mbrizzio@umich.edu>
2025-01-25 21:32:53 -0500

    Add rotation functionality to ProjectileMove

--- Assets/Scripts/Projectiles/ProjectileMove.cs
+++ Assets/Scripts/Projectiles/ProjectileMove.cs
@@ -1,24 +1,26 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class ProjectileMove : MonoBehaviour
 {
     public float speed = -10f;
-    public float rotation = 0;
+    public float directionDegrees = 0;
+    public float inPlaceRevolutionRate = 0;
 
     private float rotationAmount = 0;
     void Start()
     {
-        
+        directionDegrees = Mathf.Deg2Rad * directionDegrees;  
     }
 
     void Update()
     {
 
-      
-        transform.position = transform.position + new Vector3(0, speed, 0) * Time.deltaTime;
+
+        transform.position = transform.position + new Vector3(speed * math.sin(directionDegrees), speed * math.cos(directionDegrees), 0) * Time.deltaTime;
         transform.Rotate(new Vector3(0, 0, rotationAmount));
-        rotationAmount += rotation;
+        rotationAmount += inPlaceRevolutionRate;
         if ((speed > 0 && transform.position.y >= 5.2) || (speed < 0 && transform.position.y <= -5.2))
         {
             Destroy(gameObject);
