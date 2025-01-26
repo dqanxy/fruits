ccf29de8bc2d71409d53324e9ffb71829256f47b
Marco Brizzio <mbrizzio@umich.edu>
2025-01-25 19:55:48 -0500

    Add initial delay and fire rate functionality to PeriodicShoot.cs

--- Assets/Scripts/Projectiles/ProjectileMove.cs
+++ Assets/Scripts/Projectiles/ProjectileMove.cs
@@ -1,25 +1,40 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ProjectileMove : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = -10f;
     public float rotation = 0;
+
+    public float initialDelay = 0f;
+    public float fireRate = 5f;
+    private float countdown;
 
     private float rotationAmount = 0;
     void Start()
     {
-        
+        countdown = fireRate;
     }
 
     void Update()
     {
-        transform.position = transform.position + new Vector3(0, speed, 0) * Time.deltaTime;
-        transform.Rotate(new Vector3(0, 0, rotationAmount));
-        rotationAmount += rotation;
-        if((speed > 0 && transform.position.y >= 5.2) || (speed < 0 && transform.position.y <= -5.2))
+        if (countdown <= 0)
         {
-            Destroy(gameObject);
+            transform.position = transform.position + new Vector3(0, speed, 0) * Time.deltaTime;
+            transform.Rotate(new Vector3(0, 0, rotationAmount));
+            rotationAmount += rotation;
+            if ((speed > 0 && transform.position.y >= 5.2) || (speed < 0 && transform.position.y <= -5.2))
+            {
+                Destroy(gameObject);
+            }
+
+            countdown = fireRate;
         }
+        else
+        {
+            countdown -= 1f;
+        }
+
     }
 
 }
