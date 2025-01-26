167978268c952586c6e141ccce575f9963ed7b61
Marco Brizzio <mbrizzio@umich.edu>
2025-01-25 20:37:03 -0500

    Remove delays from ProjectileMove

--- Assets/Scripts/Projectiles/ProjectileMove.cs
+++ Assets/Scripts/Projectiles/ProjectileMove.cs
@@ -7,18 +7,22 @@ public class ProjectileMove : MonoBehaviour
     public float rotation = 0;
 
     public float initialDelay = 0f;
-    public float fireRate = 5f;
-    private float countdown;
 
     private float rotationAmount = 0;
     void Start()
     {
-        countdown = fireRate + initialDelay;
+        
     }
 
     void Update()
     {
-        if (countdown <= 0)
+        if (initialDelay > 0)
+        {
+            --initialDelay;
+        }
+        
+        
+        else
         {
             transform.position = transform.position + new Vector3(0, speed, 0) * Time.deltaTime;
             transform.Rotate(new Vector3(0, 0, rotationAmount));
@@ -28,13 +32,9 @@ public class ProjectileMove : MonoBehaviour
                 Destroy(gameObject);
             }
 
-            countdown = fireRate;
-        }
-        else
-        {
-            countdown -= 1f;
         }
 
+      
     }
 
 }
